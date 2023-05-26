38ece8b7dff17d75cd787c300ba179bc5a074cc1
Roma Kurilovich <moralle.programmer@gmail.com>
2023-05-26 18:56:16 +0300

    Add comments

--- android/app/src/main/java/com/games/MainActivity.java
+++ android/app/src/main/java/com/games/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+// Need for react-native-splash-screen
 import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
@@ -23,6 +24,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    // Need for react-native-splash-screen
     SplashScreen.show(this);
     super.onCreate(null);
   }
